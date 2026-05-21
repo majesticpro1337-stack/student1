177e541f3bc7834863327f3ff03e2a130142df33
majesticpro1337-stack <majesticpro1337@gmail.com>
2026-05-21 11:34:59 +0300

    Add files via upload
    using System;
    using System.Collections.Generic;
    using System.IO;
    using System.Linq;
    
    class Result
    {
        public static int simpleArraySum(List<int> ar)
        {
            return ar.Sum();
        }
    }
    
    class Solution
    {
        public static void Main(string[] args)
        {
            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
    
            List<int> ar = Console.ReadLine().Trim().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
    
            int result = Result.simpleArraySum(ar);
    
            Console.WriteLine(result);
        }
    }

---
+++ results/3/3/3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Result
+{
+    public static int simpleArraySum(List<int> ar)
+    {
+        return ar.Sum();
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().Trim().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+}
