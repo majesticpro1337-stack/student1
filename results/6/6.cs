50577868863ef314f8ef49647c0d33ff63fc26fe
majesticpro1337-stack <majesticpro1337@gmail.com>
2026-05-21 15:27:02 +0300

    Create 6.cs

---
+++ results/6/6.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+
+class Solution
+{
+    public static string kangaroo(int x1, int v1, int x2, int v2)
+    {
+        if (v1 > v2 && (x2 - x1) % (v1 - v2) == 0)
+        {
+            return "YES";
+        }
+        else
+        {
+            return "NO";
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().Trim().Split(' ');
+
+        int x1 = Convert.ToInt32(firstMultipleInput[0]);
+        int v1 = Convert.ToInt32(firstMultipleInput[1]);
+        int x2 = Convert.ToInt32(firstMultipleInput[2]);
+        int v2 = Convert.ToInt32(firstMultipleInput[3]);
+
+        string result = kangaroo(x1, v1, x2, v2);
+
+        Console.WriteLine(result);
+    }
+}
